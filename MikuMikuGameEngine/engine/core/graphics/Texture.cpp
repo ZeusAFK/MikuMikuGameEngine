49240d806323a4370a5ad0ad88d7f57861392a4b
Lycoris <miyabi@circle-chips.com>
2013-01-17 22:10:04 +0900

    単色ドットテクスチャをMANAGEDにしてデバイスロスト対策

--- MikuMikuGameEngine/engine/core/graphics/Texture.cpp
+++ MikuMikuGameEngine/engine/core/graphics/Texture.cpp
@@ -11,7 +11,6 @@
 #endif
 
 Texture::Texture()
-	: m_pLostSaveBuffer(NULL)
 {
 }
 
@@ -53,8 +52,8 @@ bool Texture::CreateDotColor( D3DXCOLOR color )
 
 	HRESULT hr = D3DXCreateTexture( 
 		d3dDevice,
-		16, 16, 1, D3DUSAGE_DYNAMIC, 
-		D3DFMT_A8R8G8B8, D3DPOOL_DEFAULT, &m_pTexture );
+		1, 1, 1, 0, 
+		D3DFMT_A8R8G8B8, D3DPOOL_MANAGED, &m_pTexture );
 
 	if( FAILED(hr) )
 	{
@@ -68,86 +67,10 @@ bool Texture::CreateDotColor( D3DXCOLOR color )
 
 	D3DXFillTexture( m_pTexture,FillTexColor,(LPVOID)&color );
 	
-	graphics->AddResource( this );
-
 	return true;
 }
 
 IDirect3DTexture9Ptr Texture::GetTexture()
 {
 	return m_pTexture;
-}
-
-void Texture::OnLostDevice()
-{
-	// TODO:OnLostDevice
-	if( !m_pTexture )
-	{
-		return;
-	}
-
-	OutputDebugStringFormat( _T("Texture(%p)::OnLostDevice\n"),this );
-
-	m_pTexture->GetLevelDesc(0,&m_LostDesc);
-
-	if( m_LostDesc.Pool==D3DPOOL_DEFAULT )
-	{
-		if( m_LostDesc.Usage & D3DUSAGE_DYNAMIC )
-		{
-			D3DLOCKED_RECT lock;
-			m_pTexture->LockRect(0,&lock,NULL,0);
-
-			size_t size=lock.Pitch*m_LostDesc.Height;
-
-			m_pLostSaveBuffer=new BYTE[size];
-
-			memcpy(m_pLostSaveBuffer,lock.pBits,size);
-
-			m_pTexture->UnlockRect(0);
-		}
-
-		m_pTexture.Release();
-	}
-}
-
-void Texture::OnResetDevice()
-{
-	// TODO:OnResetDevice
-	if( m_pLostSaveBuffer==NULL )
-	{
-		return;
-	}
-
-	OutputDebugStringFormat( _T("Texture(%p)::OnResetDevice\n"),this );
-
-	if( m_LostDesc.Pool==D3DPOOL_DEFAULT )
-	{
-		Graphics* graphics = Graphics::GetInstance();
-		IDirect3DDevice9Ptr pD3DDevice = graphics->GetDirect3DDevice();
-		
-		HRESULT hr;
-		hr = D3DXCreateTexture( 
-			pD3DDevice,
-			m_LostDesc.Width, m_LostDesc.Height, 1, m_LostDesc.Usage, 
-			m_LostDesc.Format, m_LostDesc.Pool,&m_pTexture );
-		//hr=pD3DDevice->CreateTexture(m_LostDesc.Width,m_LostDesc.Height,m_LostDesc.MultiSampleQuality,m_LostDesc.Usage,m_LostDesc.Format,m_LostDesc.Pool,&m_pTexture,NULL);
-		if( FAILED(hr) )
-		{
-			return;
-		}
-
-		if( m_LostDesc.Usage & D3DUSAGE_DYNAMIC )
-		{
-			D3DLOCKED_RECT lock;
-			m_pTexture->LockRect(0,&lock,NULL,0);
-
-			size_t size=lock.Pitch*m_LostDesc.Height;
-			memcpy(lock.pBits,m_pLostSaveBuffer,size);
-
-			m_pTexture->UnlockRect(0);
-
-			delete[] m_pLostSaveBuffer;
-			m_pLostSaveBuffer = NULL;
-		}
-	}
 }
--- MikuMikuGameEngine/engine/core/graphics/Texture.h
+++ MikuMikuGameEngine/engine/core/graphics/Texture.h
@@ -4,7 +4,7 @@
 
 #include "../ResourceManager.h"
 
-class Texture : public UnmanagedResource , public IResource
+class Texture : public IResource
 {
 private:
 	IDirect3DTexture9Ptr m_pTexture;
@@ -18,14 +18,6 @@ public:
 	bool CreateDotColor( D3DXCOLOR color );
 
 	IDirect3DTexture9Ptr GetTexture();
-
-private:
-	D3DSURFACE_DESC m_LostDesc;
-	BYTE* m_pLostSaveBuffer;
-
-public:
-	virtual void OnLostDevice();
-	virtual void OnResetDevice();
 };
 
 typedef std::tr1::shared_ptr< Texture > TexturePtr;
